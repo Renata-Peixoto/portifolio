244140e3eff6e211fbf816e56663c8f5de06c866
Renata-Peixoto <www.renata555@gmail.com>
2023-07-01 10:42:29 -0300

    front end

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var TrandingSlider = new Swiper('.tranding-slider', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+});
+function ativaletra(elemento) {
+  const arrTexto = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  arrTexto.forEach((Letra, i) => {
+    setTimeout(() => {
+      elemento.innerHTML += letra;
+    }, 75 * i);
+  })
+}
+const titulo = document.querySelector('.title bg-text');
+ativaletra(titulo);
+
+
